316da1d19b3d8d5d8c05be0cd59b7e9d80fef1ca
Tamara Tell <2telltamara@gmail.com>
2023-04-17 07:30:44 -0400

    added index.js and refactored functions from breedFetcher.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,24 +4,30 @@ const request = require("request");
 // https://api.thecatapi.com/v1/images/search?breed_id=beng will show bengal cat as results
 //https://api.thecatapi.com/v1/breeds/search will allow us to search breeds
 
+const breedName = process.argv.slice(2);
 
-const getBreedUrl = () => {
-  const baseUrl = 'https://api.thecatapi.com/v1/breeds/search?';
-  const breed = process.argv.slice(2);
-  const queryString = `q=${breed}`;
-  const url = `${baseUrl}${queryString}`;
-  return url;
-};
+// const getBreedUrl = () => {
+//   const baseUrl = 'https://api.thecatapi.com/v1/breeds/search?';
+//   const breedName = process.argv.slice(2);
+//   const queryString = `q=${breedName}`;
+//   const url = `${baseUrl}${queryString}`;
+//   return url;
+// };
 
-request(getBreedUrl(), (error, response, body) => {
-  const data = JSON.parse(body);
-  try {
-    //tryStatements
-    const breedInformation = (data[0]).description;
-    console.log(breedInformation);
-  } catch (error) {
-    //catchStatements
-    console.error('Error:', error.message);
+
+const fetchBreedDescription = (breedName, callback => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      console.log(error.message, null);
+    };
+    const desc = JSON.parse(body);
+    if (desc[0]) {
+      console.log(null, desc[0].description);
+    };
   }
-
+  );
 });
+
+fetchBreedDescription();
+
+module.exports = fetchBreedDescription;
